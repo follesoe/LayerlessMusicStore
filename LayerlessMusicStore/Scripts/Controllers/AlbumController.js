38b562cd3f94096372518c74a6f7408e73a6a0b4
Jonas Folleso <jonas@follesoe.no>
2011-06-04 13:58:16 +0200

    Can now edit and delete albums.

--- LayerlessMusicStore/Scripts/Controllers/AlbumController.js
+++ LayerlessMusicStore/Scripts/Controllers/AlbumController.js
@@ -12,11 +12,33 @@
         });
     });
 
+    this.get("#/admin/album/edit/:id", function (context) {
+        rest.loadData("/indexes/dynamic/genre", function (genreResponse) {
+            rest.loadData("/indexes/dynamic/album?query=Id:" + context.params["id"], function (albumResponse) {
+                var album = albumResponse.Results[0];
+                album.genres = genreResponse.Results;
+                context.partial("/Content/Views/Admin/Album/Edit.html", album);
+            });
+        });
+    });
+
     this.post("#/admin/album/save", function (context) {
         var album = this.params;
         album.Genre.Name = $(context.target).find("select :selected").text();
         album.AlbumArtUrl = album.AlbumArtUrl || "/Content/Images/placeholder.gif";
         $.post("/App/Save/album/", JSON.stringify(album), function () {
+            context.redirect("#/admin/album");
+        });
+    });
+
+    this.get("#/admin/album/delete/:id", function (context) {
+        rest.loadData("/indexes/dynamic/album?query=Id:" + context.params["id"], function (response) {
+            context.partial("/Content/Views/Admin/Album/Delete.html", response.Results[0]);
+        });
+    });
+
+    this.post("#/admin/album/delete/:id", function (context) {
+        $.post("/App/Delete/" + context.params["id"], function () {
             context.redirect("#/admin/album");
         });
     });
