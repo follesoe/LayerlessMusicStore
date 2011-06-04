cf9768faf5704e3bd56b7537fa442aa612352928
Jonas Folleso <jonas@follesoe.no>
2011-06-04 15:24:31 +0200

    Lists latest album on front-page.

--- LayerlessMusicStore/Scripts/Controllers/App.js
+++ LayerlessMusicStore/Scripts/Controllers/App.js
@@ -3,8 +3,13 @@
     this.use(Sammy.NestedParams);
 
     this.get("#/", function (context) {
-        rest.loadData("/indexes/dynamic/genre", function (response) {
-            context.partial("/Content/Views/Main.html", { genres: response.Results });
+        rest.loadData("/indexes/dynamic/genre", function (genreResponse) {
+            rest.loadData("/indexes/Raven/DocumentsByEntityName?query=Tag:album&start=0&pageSize=5", function (albumResponse) {
+                context.partial("/Content/Views/Main.html", {
+                    genres: genreResponse.Results,
+                    albums: albumResponse.Results
+                });
+            });
         });
     });
 
